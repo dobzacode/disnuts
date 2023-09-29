5889726ab006580bb9afdb8b6b525308c862fa40
dobzacode <ckittel67@gmail.com>
2023-09-29 12:21:26 +0200

    add a dropdown menu

--- components/user/UserSnippet.tsx
+++ components/user/UserSnippet.tsx
@@ -1,11 +1,40 @@
+"use client";
+
 import { Session } from "next-auth";
 import Avatar from "../Avatar";
+import Icon from "@mdi/react";
+import { mdiArrowDown } from "@mdi/js";
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 export default function UserSnippet({ session }: { session: Session }) {
+  const [isShown, setIsShown] = useState<Boolean>(false);
+
   return (
-    <div className="bg-white gap-extra-small text-body font-medium rounded-full flex justify-center items-center px-sub-medium brutalism-border py-1 border-primary80">
-      <Avatar></Avatar>
-      <p>{session?.user?.name}</p>
+    <div className="flex flex-col relative">
+      <button
+        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          e.stopPropagation();
+          setIsShown(!isShown);
+        }}
+        className="bg-white gap-small text-body font-medium rounded-full flex justify-center items-center px-sub-medium brutalism-border py-1 border-primary80 relative z-20"
+      >
+        <div className="flex items-center">
+          <Avatar></Avatar>
+          <p>{session?.user?.name}</p>
+        </div>
+        <Icon path={mdiArrowDown} size={1.5}></Icon>
+      </button>
+      <CSSTransition
+        in={isShown as boolean}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <ul className="fade-enter-done absolute top-12 bg-white gap-small text-body font-medium rounded-b-sub-large flex flex-col items-center px-sub-medium brutalism-border  border-primary80 w-full z-10 pb-small pt-sub-large ">
+          <li>Sign-out</li>
+        </ul>
+      </CSSTransition>
     </div>
   );
 }
