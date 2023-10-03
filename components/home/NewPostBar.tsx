44b0d55ce7173b30fd96883986afcbb40c8fc81b
dobzacode <ckittel67@gmail.com>
2023-10-03 14:25:27 +0200

    done

--- components/home/NewPostBar.tsx
+++ components/home/NewPostBar.tsx
@@ -1,16 +1,20 @@
+import Link from "next/link";
 import Avatar from "../Avatar";
 
 export default function NewPostBar({}) {
   return (
-    <div className="flex gap-extra-small bg-primary10 p-extra-small brutalism-border border-primary80 rounded-small cursor-pointer">
+    <Link
+      href="/post/create"
+      className="flex gap-extra-small bg-primary10 p-extra-small brutalism-border border-primary80 rounded-small cursor-pointer w-fit"
+    >
       <div className="bg-white rounded-small p-extra-small">
         <Avatar></Avatar>
       </div>
       <input
-        className="body focus:outline-none bg-white rounded-small px-small placeholder:text-neutral80"
+        className="body focus:outline-none bg-white rounded-small px-small placeholder:text-neutral80 cursor-pointer"
         type="text"
         placeholder="New publication"
       />
-    </div>
+    </Link>
   );
 }
