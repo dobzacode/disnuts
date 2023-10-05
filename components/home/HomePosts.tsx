b3d16539668b7869f1ebb47ecbecd2a0e7ab290f
dobzacode <ckittel67@gmail.com>
2023-10-05 19:44:06 +0200

    add post mock in dev mode

--- components/home/HomePosts.tsx
+++ components/home/HomePosts.tsx
@@ -3,13 +3,17 @@ import prisma from "@/prisma/client";
 import { Post } from "@prisma/client";
 import { v4 as uuidv4 } from "uuid";
 import PostSkeleton from "../post/PostSkeleton";
+import postMock from "@/mocks/POST_MOCK.json";
 
 export default async function HomePosts({}) {
-  const data: Post[] = await prisma.post.findMany();
+  const posts: Post[] =
+    process.env.NODE_ENV === "production"
+      ? await prisma.post.findMany()
+      : (postMock as any);
 
   return (
     <>
-      {data.map((post) => {
+      {posts.map((post) => {
         return <PostBar post={post} key={uuidv4()}></PostBar>;
       })}
     </>
--- components/profile/ProfilePosts.tsx
+++ components/profile/ProfilePosts.tsx
@@ -4,12 +4,17 @@ import { v4 as uuidv4 } from "uuid";
 import PostBar from "../post/PostBar";
 import { Session } from "next-auth";
 
+import postMock from "@/mocks/POST_MOCK.json";
+
 export default async function ProfilePosts({
   session,
 }: {
   session: Session | null;
 }) {
-  const { posts }: { posts: Post[] } = await getUserPosts(session);
+  const posts: Post[] =
+    process.env.NODE_ENV === "production"
+      ? await getUserPosts(session)
+      : (postMock as any);
 
   return (
     <>
