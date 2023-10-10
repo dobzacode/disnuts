ee0f0452612a7ff056dd3d1b001a7448844ab1c3
dobzacode <ckittel67@gmail.com>
2023-10-10 11:36:53 +0200

    put comment in client side

--- components/post/comment/CommentBar.tsx
+++ components/post/comment/CommentBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Avatar from "@/components/ui/Avatar";
 import P from "@/components/ui/text/P";
 import { CommentDetail } from "@/interface/interface";
@@ -5,37 +7,56 @@ import { getDateDifference } from "@/utils/utils";
 import { mdiArrowDown, mdiArrowUp, mdiCommentOutline } from "@mdi/js";
 import Icon from "@mdi/react";
 import { Comment, Post, Vote } from "@prisma/client";
+import { useEffect, useState } from "react";
+import PostSkeleton from "../PostSkeleton";
 
-export default async function CommentBar({
+export default function CommentBar({
   content,
   comment_id,
 }: {
   content: string;
   comment_id: string;
 }) {
-  const url: string = `http://localhost:3000/api/comments?comment_id=${comment_id}`;
+  const [comment, setComment] = useState<CommentDetail | null>(null);
+  const [upvotes, setUpvotes] = useState<Vote[] | []>([]);
+  const [downvotes, setDownvotes] = useState<Vote[] | []>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const res = await fetch(url, { cache: "no-store" });
+  useEffect(() => {
+    const fetchComment = async () => {
+      const url: string = `/api/comments?comment_id=${comment_id}`;
 
-  const { comment }: { comment: CommentDetail } = await res.json();
+      const res = await fetch(url);
 
-  const upvotes = comment.votes?.filter((vote: Vote) => vote.type === "UPVOTE");
-  const downvotes = comment.votes?.filter(
-    (vote: Vote) => vote.type === "DOWNVOTE",
-  );
+      const { comment: data }: { comment: CommentDetail } = await res.json();
+
+      setComment(data);
+
+      setUpvotes(data.votes?.filter((vote: Vote) => vote.type === "UPVOTE"));
+
+      setDownvotes(
+        data.votes?.filter((vote: Vote) => vote.type === "DOWNVOTE"),
+      );
+
+      setIsLoading(false);
+    };
+    fetchComment();
+  });
+
+  if (isLoading) return <PostSkeleton></PostSkeleton>;
 
   return (
     <section className="brutalism-border primary-hover relative flex  h-[14rem] w-full rounded-small border-primary80">
       <div className="flex flex-col items-center gap-extra-small  rounded-l-small bg-primary10 p-small">
         <Icon path={mdiArrowUp} size={1}></Icon>
-        <P>{comment.votes ? upvotes.length - downvotes.length : 0}</P>
+        <P>{comment?.votes ? upvotes.length - downvotes.length : 0}</P>
         <Icon path={mdiArrowDown} size={1}></Icon>
       </div>
       <div className="flex flex-col justify-between gap-extra-small p-small">
         <div className="caption flex items-center gap-extra-small">
           <div className="absolute -left-large top-small flex flex-col items-center">
             <Avatar
-              src={comment.author.image}
+              src={comment?.author.image}
               size={5}
               className="rounded-small"
             ></Avatar>
@@ -43,9 +64,11 @@ export default async function CommentBar({
           </div>
 
           <P type="caption">{`Posted by u/${
-            comment.author.name ? comment.author.name : "deleted"
+            comment?.author.name ? comment?.author.name : "deleted"
           }`}</P>
-          <P type="caption">{getDateDifference(comment.createdAt)}</P>
+          <P type="caption">
+            {comment?.createdAt && getDateDifference(comment?.createdAt)}
+          </P>
         </div>
         <P>{content}</P>
         <div className="flex gap-extra-small">
