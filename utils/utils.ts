6490f7993306b615ce8ecae9f51ae8a121976123
dobzacode <ckittel67@gmail.com>
2023-10-27 18:02:51 +0200

    fix url in upload media utils

--- utils/utils.ts
+++ utils/utils.ts
@@ -90,7 +90,7 @@ export async function zeroShotClassify(input: string[], parameters: string[]) {
 
 export async function uploadMedia(file: File, to: string, id: string) {
   try {
-    const res = await fetch(`${BASE_URL}/api/media?to=${to}&id=${id}`, {
+    const res = await fetch(`/api/media?to=${to}&id=${id}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
